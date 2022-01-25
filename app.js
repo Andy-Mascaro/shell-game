c1248db3cb08cdc93446e9f295b0576b57061c6c
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-25 11:49:45 -0800

    added js

--- app.js
+++ app.js
@@ -4,17 +4,47 @@ const ball2 = document.getElementById('ball2');
 const ball3 = document.getElementById('ball3');
 
 const button1 = document.getElementById('shell1-button');
-const button2 = document.getElementById('shell2-button');
-const button3 = document.getElementById('shell3-button');
+
 
 const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
 const totalSpan = document.getElementByUd('total');
 
+let wins = 0;
+let total = 0;
 
-//const counter h4= document.getElement('counter');
 
 // set event listeners 
+
+button1.addEventListener('click', ()=> {
+
+    ball1.classList.remove('reveal');
+    ball2.classList.remove('reveal');
+    ball3.classList.remove('reveal');
+
+    total++;
+
+    const ballLocation = Math.ceil(Math.random () * 3);
+
+    if (ballLocation === 1) {
+        ball1.classList.add('reveal');
+        wins++;
+
+    } else if (ballLocation === 2) {
+        ball2.classList.add('reveal');
+
+    } else { 
+        ball3.classList.add('reveal');
+
+    }
+
+    winSpan.textContent = wins;
+    totalSpan.testContent = total;
+    lossSpan.textContent = total - wins;
+
+
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
